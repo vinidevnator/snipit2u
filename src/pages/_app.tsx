fd6980c786da5368548e89844f09f7c7b3ef53bd
Vinícius Rosa <viniciusrosa3525@gmail.com>
2024-02-06 10:58:32 -0300

    Add adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,18 @@ import { ChakraProvider, GlobalStyle } from "@chakra-ui/react";
 import { defaultTheme } from "../theme";
 import { useEffect } from "react";
 import TagManager from "react-gtm-module";
+import Script from "next/script";
 
 function Snipit2u({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: "G-2G6N6XDNP2" });
   }, []);
+
+  <Script
+    async
+    strategy="afterInteractive"
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3721444151635807"
+  />;
 
   return (
     <ChakraProvider theme={defaultTheme}>
