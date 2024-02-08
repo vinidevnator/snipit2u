8c3dc7dac77a93d9dee9481a85643e0dcb92d3b7
Vinícius Rosa <viniciusrosa3525@gmail.com>
2024-02-07 21:01:55 -0300

    Improve the header section

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,37 +1,35 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 export function Header() {
-  const router = useRouter();
-
-  function handleOnClick() {
-    router.push("/");
-  }
   return (
-    <Flex justify="space-between" mt="2">
-      <Box>
-        <Button
-          color="white"
-          type="submit"
-          variant="link"
-          onClick={handleOnClick}
-        >
-          Home
-        </Button>
+    <Flex mt="2">
+      <Box padding="2" borderRadius="8" backdropBlur="base" border="2px">
+        <Text fontSize="xl" fontWeight="bold" color="red" textAlign="center">
+          snipit2u
+        </Text>
       </Box>
-      <Box>
-        <Link href="/privacy">
-          <Button type="submit" color="white" variant="link" h="/privacy">
-            Privacy
+      <Flex w="100%" justify="space-between" align="center">
+        <Box ml="2">
+          <Link href="/">
+            <Button color="white" type="submit" variant="link">
+              Home
+            </Button>
+          </Link>
+        </Box>
+        <Box>
+          <Link href="/privacy">
+            <Button type="submit" color="white" variant="link">
+              Privacy
+            </Button>
+          </Link>
+        </Box>
+        <Box>
+          <Button disabled type="submit" variant="link">
+            Donate
           </Button>
-        </Link>
-      </Box>
-      <Box>
-        <Button disabled type="submit" variant="link">
-          Donate
-        </Button>
-      </Box>
+        </Box>
+      </Flex>
     </Flex>
   );
 }
--- src/pages/privacy/index.tsx
+++ src/pages/privacy/index.tsx
@@ -1,9 +1,14 @@
 import { Box, Container } from "@chakra-ui/react";
+import Head from "next/head";
+
 import { Header } from "../../components/Header";
 
 function Privacy() {
   return (
     <Container>
+      <Head>
+        <title>Snipit2U - Privacy Policy!</title>
+      </Head>
       <Header />
       <Box mt="20">
         Only collects data analitycs from Google and Ads to improve earnings.
